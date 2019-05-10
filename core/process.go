1feacbfc7a58d3514025fb43657db6c5ff8697c5
Omollo Erick <ericotieno99@gmail.com>
2019-05-10 14:01:29 +0300

    test

--- core/process.go
+++ core/process.go
@@ -62,24 +62,25 @@ func processRequest(conn net.Conn, b []byte, byteLen int, clientJobs chan models
 
 	// Transmission Reason Specific data – 1 byte
 	trsd := 0
-	if deviceData.TransmissionReason == 255 {
-		byteReader.Seek(17, 0)
-		specific := make([]byte, 1)
-		byteReader.Read(specific)
+	// if deviceData.TransmissionReason == 255 {
+	byteReader.Seek(17, 0)
+	specific := make([]byte, 1)
+	byteReader.Read(specific)
 
-		var a = uint(specific[0])
-		fmt.Println(int8(specific[0]))
-		failsafe := isKthBitSet(a, 1)
-		fmt.Println(failsafe)
-		failsafe2 := isKthBitSet(a, 2)
-		fmt.Println(failsafe2)
-		failsafe3 := isKthBitSet(a, 3)
-		fmt.Println(failsafe3)
-		failsafe4 := isKthBitSet(a, 4)
-		fmt.Println(failsafe4)
+	var a = uint(specific[0])
+	fmt.Printf("%08b\n", int8(specific[0]))
 
-		trsd = int(a)
-	}
+	failsafe := isKthBitSet(a, 1)
+	fmt.Println(failsafe)
+	failsafe2 := isKthBitSet(a, 2)
+	fmt.Println(failsafe2)
+	failsafe3 := isKthBitSet(a, 3)
+	fmt.Println(failsafe3)
+	failsafe4 := isKthBitSet(a, 4)
+	fmt.Println(failsafe4)
+
+	trsd = int(a)
+	// }
 	deviceData.TransmissionReasonSpecificData = trsd
 
 	// Number of satellites used (from GPS) – 1 byte
