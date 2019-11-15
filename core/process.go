9ffa3105a62aa23a7ca70b68a1c5ac36c080cb97
Omollo Erick <ericotieno99@gmail.com>
2019-11-15 15:53:48 +0300

    reduced the number of workers to 10

--- core/process.go
+++ core/process.go
@@ -18,7 +18,7 @@ import (
 	"github.com/ekas-data-portal/models"
 )
 
-const queueLimit = 100
+const queueLimit = 10
 
 // HandleRequest Handles incoming requests.
 func HandleRequest(conn net.Conn) {
