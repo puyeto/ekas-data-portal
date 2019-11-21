e1826e43418d96460978d0bd6a829222d325aa42
Omollo Erick <ericotieno99@gmail.com>
2019-11-21 10:52:16 +0300

    isolate id

--- core/process.go
+++ core/process.go
@@ -213,9 +213,9 @@ func processRequest(conn net.Conn, b []byte, byteLen int) {
 	clientJobs <- models.ClientJob{deviceData, conn}
 	//}
 
-	// if deviceData.DeviceID == 1067898181 {
-	fmt.Println(deviceData)
-	// }
+	if deviceData.DeviceID == 1192729163 {
+		fmt.Println(deviceData)
+	}
 
 	// send data to ntsa
 	// go sendToNTSA(deviceData)
