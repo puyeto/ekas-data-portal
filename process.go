3e72ed930d76ff12f96a756bf965952585b40683
Omollo Erick <ericotieno99@gmail.com>
2020-02-03 14:25:34 +0300

    Check device status

--- process.go
+++ process.go
@@ -200,15 +200,15 @@ func processRequest(conn net.Conn, b []byte, byteLen int) {
 	deviceData.DateTime = time.Date(deviceData.UTCTimeYear, time.Month(deviceData.UTCTimeMonth), deviceData.UTCTimeDay, deviceData.UTCTimeHours, deviceData.UTCTimeMinutes, deviceData.UTCTimeSeconds, 0, time.UTC)
 	deviceData.DateTimeStamp = deviceData.DateTime.Unix()
 
-	// if checkIdleState(deviceData) != "idle3" {
-	clientJobs <- models.ClientJob{deviceData, conn}
-	//}
+	if checkIdleState(deviceData) != "idle3" {
+		clientJobs <- models.ClientJob{deviceData, conn}
+	}
 
-	// if deviceData.DeviceID == 1120119565 {
-	// 	deviceData.DeviceID = 1193313879
-	// 	deviceData.GroundSpeed = 0
-	// 	clientJobs <- models.ClientJob{deviceData, conn}
-	// }
+	if deviceData.DeviceID == 1217395225 {
+		deviceData.DeviceID = 1162212238
+		deviceData.GroundSpeed = 0
+		clientJobs <- models.ClientJob{deviceData, conn}
+	}
 
 	// send data to ntsa
 	// go sendToNTSA(deviceData)
