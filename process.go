3f70026546271df66971dc8644516ca2ad54e953
Omollo Erick <ericotieno99@gmail.com>
2020-03-05 15:49:29 +0300

    Remove data manipulation

--- process.go
+++ process.go
@@ -205,14 +205,11 @@ func processRequest(conn net.Conn, b []byte, byteLen int) {
 	clientJobs <- models.ClientJob{deviceData, conn}
 	// }
 
-	if deviceData.DeviceID == 1254555524 {
-		deviceData.DeviceID = 1131210222
-		clientJobs <- models.ClientJob{deviceData, conn}
-		deviceData.DeviceID = 1254205030
-		clientJobs <- models.ClientJob{deviceData, conn}
-		deviceData.DeviceID = 1177790866
-		clientJobs <- models.ClientJob{deviceData, conn}
-	}
+	// if deviceData.DeviceID == 1254555524 {
+	// 	deviceData.GroundSpeed = 0
+	// 	deviceData.DeviceID = 1131210222
+	// 	clientJobs <- models.ClientJob{deviceData, conn}
+	// }
 
 	// send data to ntsa
 	// go sendToNTSA(deviceData)
