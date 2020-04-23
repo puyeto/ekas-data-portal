6d9afa4f92e156fae51e4f5f1b90bc868de1f918
Omollo Erick <ericotieno99@gmail.com>
2020-04-23 12:23:42 +0300

    URL changes

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func init() {
 	//Do NOT CLOSE the db as it is ment to be long lasting
 	core.DBCONN = core.DBconnect("ekas_portal")
 	core.DBCONDATA = core.DBconnect("ekas_portal_data")
-	core.MongoDB = core.InitializeMongoDB("mongodb://root:safcom2012@10.138.172.89:27017/?authSource=admin", "ekas_portal")
+	core.MongoDB = core.InitializeMongoDB("mongodb://root:safcom2012@172.17.0.1:27017/?authSource=admin", "ekas_portal")
 	err := core.InitializeRedis()
 	if err != nil {
 		// panic(err)
