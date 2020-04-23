e8bc5e5efd4ef84622b0ad4254178131398391c7
Omollo Erick <ericotieno99@gmail.com>
2020-04-23 08:17:16 +0300

    Return mongo err

--- process.go
+++ process.go
@@ -249,6 +249,7 @@ func LogToMongoDB(m models.DeviceData) error {
 	collection := core.MongoDB.Collection("data_" + strconv.FormatInt(int64(m.DeviceID), 10))
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	_, err := collection.InsertOne(ctx, m)
+	fmt.Println(err)
 	return err
 }
 
