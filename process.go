eaf1aaa3d8050322577916fe05e4c2b662193209
Omollo Erick <ericotieno99@gmail.com>
2020-04-23 08:27:03 +0300

    Adjusted mongodb timeout

--- process.go
+++ process.go
@@ -247,7 +247,7 @@ func generateResponses(clientJobs chan models.ClientJob) {
 // LogToMongoDB ...
 func LogToMongoDB(m models.DeviceData) error {
 	collection := core.MongoDB.Collection("data_" + strconv.FormatInt(int64(m.DeviceID), 10))
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	_, err := collection.InsertOne(ctx, m)
 	fmt.Println(err)
 	return err
