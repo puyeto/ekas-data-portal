94d65ebf1362df1942170ea927b7eafe5e67626a
Omollo Erick <ericotieno99@gmail.com>
2020-06-13 10:42:24 +0300

    downgraded go-redis to v7 added go mods

--- core/redis.go
+++ core/redis.go
@@ -1,12 +1,11 @@
 package core
 
 import (
-	"context"
 	"encoding/json"
 	"time"
 
 	"github.com/ekas-data-portal/models"
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v7"
 )
 
 var (
@@ -15,7 +14,6 @@ var (
 
 // InitializeRedis ...
 func InitializeRedis() error {
-	ctx, _ := context.WithCancel(context.Background())
 	// if os.Getenv("GO_ENV") != "production" {
 	// 	redisURL = "db-redis-cluster-do-user-4666162-0.db.ondigitalocean.com:25061"
 	// }
@@ -27,7 +25,7 @@ func InitializeRedis() error {
 
 	redisClient = redis.NewClient(opt)
 
-	ping, err := redisClient.Ping(ctx).Result()
+	ping, err := redisClient.Ping().Result()
 	if err == nil && len(ping) > 0 {
 		Logger.Info("Connected to Redis")
 		return nil
@@ -39,132 +37,114 @@ func InitializeRedis() error {
 // GetValue ...
 func GetValue(key string) (interface{}, error) {
 	var deserializedValue interface{}
-	ctx, _ := context.WithCancel(context.Background())
-	serializedValue, err := redisClient.Get(ctx, key).Result()
+	serializedValue, err := redisClient.Get(key).Result()
 	json.Unmarshal([]byte(serializedValue), &deserializedValue)
 	return deserializedValue, err
 }
 
 // SetValue ...
 func SetValue(key string, value interface{}) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
 	serializedValue, _ := json.Marshal(value)
-	err := redisClient.Set(ctx, key, string(serializedValue), 0).Err()
+	err := redisClient.Set(key, string(serializedValue), 0).Err()
 	return true, err
 }
 
 // GetLastSeenValue ...
 func GetLastSeenValue(key string) (models.DeviceData, error) {
 	var deserializedValue models.LastSeenStruct
-	ctx, _ := context.WithCancel(context.Background())
-	serializedValue, err := redisClient.Get(ctx, key).Result()
+	serializedValue, err := redisClient.Get(key).Result()
 	json.Unmarshal([]byte(serializedValue), &deserializedValue)
 	return deserializedValue.DeviceData, err
 }
 
 // SetValueWithTTL ...
 func SetValueWithTTL(key string, value interface{}, ttl int) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
 	serializedValue, _ := json.Marshal(value)
-	err := redisClient.Set(ctx, key, string(serializedValue), time.Duration(ttl)*time.Second).Err()
+	err := redisClient.Set(key, string(serializedValue), time.Duration(ttl)*time.Second).Err()
 	return true, err
 }
 
 // HSet ...
 func HSet(key string, value interface{}) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
 	serializedValue, _ := json.Marshal(value)
-	err := redisClient.HSet(ctx, key, string(serializedValue), 0).Err()
+	err := redisClient.HSet(key, string(serializedValue), 0).Err()
 	return true, err
 }
 
 // HGetAll ...
 func HGetAll(key string) (interface{}, error) {
 	// var deserializedValue interface{}
-	ctx, _ := context.WithCancel(context.Background())
-	serializedValue, err := redisClient.HGetAll(ctx, key).Result()
+	serializedValue, err := redisClient.HGetAll(key).Result()
 	// json.Unmarshal([]byte(serializedValue), &deserializedValue)
 	return serializedValue, err
 }
 
 // RPush ...
 func RPush(key string, valueList []string) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
-	err := redisClient.RPush(ctx, key, valueList).Err()
+	err := redisClient.RPush(key, valueList).Err()
 	return true, err
 }
 
 // RpushWithTTL ...
 func RpushWithTTL(key string, valueList []string, ttl int) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
-	err := redisClient.RPush(ctx, key, valueList, ttl).Err()
+	err := redisClient.RPush(key, valueList, ttl).Err()
 	return true, err
 }
 
 // LRange ...
 func LRange(key string) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
-	err := redisClient.LRange(ctx, key, 0, -1).Err()
+	err := redisClient.LRange(key, 0, -1).Err()
 	return true, err
 }
 
 // ListLength ...
 func ListLength(key string) int64 {
-	ctx, _ := context.WithCancel(context.Background())
-	return redisClient.LLen(ctx, key).Val()
+	return redisClient.LLen(key).Val()
 }
 
 // Publish ...
 func Publish(channel string, message string) {
-	ctx, _ := context.WithCancel(context.Background())
-	redisClient.Publish(ctx, channel, message)
+	redisClient.Publish(channel, message)
 }
 
 // GetKeyListByPattern ...
 func GetKeyListByPattern(pattern string) []string {
-	ctx, _ := context.WithCancel(context.Background())
-	return redisClient.Keys(ctx, pattern).Val()
+	return redisClient.Keys(pattern).Val()
 }
 
 // IncrementValue ...
 func IncrementValue(key string) int64 {
-	ctx, _ := context.WithCancel(context.Background())
-	return redisClient.Incr(ctx, key).Val()
+	return redisClient.Incr(key).Val()
 }
 
 // DelKey ...
 func DelKey(key string) error {
-	ctx, _ := context.WithCancel(context.Background())
-	return redisClient.Del(ctx, key).Err()
+	return redisClient.Del(key).Err()
 }
 
 // ListKeys ...
 func ListKeys(key string) ([]string, error) {
-	ctx, _ := context.WithCancel(context.Background())
-	return redisClient.Keys(ctx, key).Result()
+	return redisClient.Keys(key).Result()
 }
 
 // SAdd Add values to a set...
 func SAdd(key string, members interface{}) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
 	serializedValue, _ := json.Marshal(members)
-	err := redisClient.SAdd(ctx, key, string(serializedValue), 0).Err()
+	err := redisClient.SAdd(key, string(serializedValue), 0).Err()
 	return true, err
 }
 
 // LPush push values to the beggining of alist ...
 func LPush(key string, members interface{}) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
 	serializedValue, _ := json.Marshal(members)
-	err := redisClient.LPush(ctx, key, string(serializedValue)).Err()
+	err := redisClient.LPush(key, string(serializedValue)).Err()
 	return true, err
 }
 
 // ZAdd ...
 func ZAdd(key string, score int64, members interface{}) (bool, error) {
-	ctx, _ := context.WithCancel(context.Background())
 	serializedValue, _ := json.Marshal(members)
-	err := redisClient.ZAdd(ctx, key, &redis.Z{
+	err := redisClient.ZAdd(key, &redis.Z{
 		Score:  float64(score),
 		Member: string(serializedValue),
 	}).Err()
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ekas-data-portal
+
+go 1.13
+
+require (
+	github.com/go-redis/redis/v7 v7.4.0
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
+	github.com/sirupsen/logrus v1.6.0
+	go.mongodb.org/mongo-driver v1.3.4
+)
