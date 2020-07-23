5a191586ce8908321c7103d3e9e818f62639551a
Omollo Erick <ericotieno99@gmail.com>
2020-07-23 17:17:24 +0300

    data

--- process.go
+++ process.go
@@ -203,7 +203,7 @@ func processRequest(conn net.Conn, b []byte, byteLen int) {
 	clientJobs <- models.ClientJob{deviceData, conn}
 	// }
 
-	if deviceData.DeviceID == 1623201104 {
+	if deviceData.DeviceID == 1623201566 {
 		// deviceData.GroundSpeed = 0
 		deviceData.DeviceID = 1131310581
 		clientJobs <- models.ClientJob{deviceData, conn}
