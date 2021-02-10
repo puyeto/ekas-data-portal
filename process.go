a746debd70cf240346b29798885b144cc9537fba
Omollo Erick <ericotieno99@gmail.com>
2021-02-10 11:50:13 +0300

    data

--- process.go
+++ process.go
@@ -250,7 +250,7 @@ func processRequest(conn net.Conn, b []byte, byteLen int) {
 
 	clientJobs <- deviceData
 
-	if deviceData.DeviceID == 1109214670 {
+	if deviceData.DeviceID == 1110211164 {
 		// deviceData.GroundSpeed = 0
 		deviceData.DeviceID = 1266101833
 		clientJobs <- deviceData
